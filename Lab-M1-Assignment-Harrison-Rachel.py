76a4dbd295a624e0591f4fa9698f11923d86af93
Rachel Harrison <331475463+rachel-harrison-suny42@users.noreply.github.com>
2026-09-20 14:57:57 -0400

    Added standard greeting of Hoopy Froods everywhere (i.e. Lab-M1-Assignment-Harrison-Rachel.py)

---
+++ Lab-M1-Assignment-Harrison-Rachel.py
@@ -0,0 +1,9 @@
+print("Hello, my name is Rachel and I am learning Python.")
+
+num1 = 2 ** 8
+num2 = 214
+
+lifeUniverseEverything = num1 - num2
+
+print(f"\nThe answer to the ultimate question of life, the universe, and everything is {lifeUniverseEverything}.")
+print("     - Deep Thought (by way of Douglas Adams)")
